11ad6fe02c07eda1d650cb0aab5f0447b66890ec
Orvar Segerström <orvarsegerstrom@gmail.com>
2024-11-25 08:49:44 +0100

    Fix node item when non-node runtime

--- functions/_tide_item_node.fish
+++ functions/_tide_item_node.fish
@@ -1,4 +1,6 @@
 function _tide_item_node
+    # Return if node is not installed (eg Bun)
+    command -v node > /dev/null || return
     if path is $_tide_parent_dirs/package.json
         node --version | string match -qr "v(?<v>.*)"
         _tide_print_item node $tide_node_icon' ' $v
